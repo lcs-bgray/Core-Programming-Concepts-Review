78bcef8ea2502e662e25a71014072e1c415bb2a7
lcs-bgray <bgray@lcs.on.ca>
2018-01-23 11:07:52 -0500

    Question #1

--- Core-Programming-Concepts-Review.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Review.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -23,15 +23,15 @@ import Foundation
 // Revise code below to answer question 1
 
 // Transformation parameters
-let updown = 5.0
-let lr = 2.0
-let stretchvalue = 0.5
+let vertical = 5.0
+let horizontal = 2.0
+let verticalScale = 0.5
 
 // Set the independent variable value
 let input = 100.0
 
 // Calculate the dependent variable value
-let y = stretchvalue * pow(input - lr, 2) + updown
+let output = verticalScale * pow(input - horizontal, 2) + vertical
 
 /*:
  In other words:
